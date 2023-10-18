383b374aac1a9cd11b0410e9601d12b84e225d9a
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-10-18 16:14:48 +0500

    paralax

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+    // relativeInput: true
+});
+
+$(".reclam-slider").on("init reInit afterChange", function (event, slick) {
+    var sCurr = slick.slickCurrentSlide() <= 8 ? '0' + (slick.slickCurrentSlide() + 1) : slick.slickCurrentSlide() + 1;
+    var sCount = slick.slideCount <= 8 ? '0' + slick.slideCount : slick.slideCount;
+    $(".reclam__counter").html('<span>' + sCurr + '</span>' + "/" + sCount);
+});
+$('.reclam-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    appendArrows: $(".reclam-controls"),
+    centerMode: true,
+    variableWidth: true,
+    adaptiveHeight: true
+});
+$('.clients-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    // centerMode: true,
+    // variableWidth: true,
+    adaptiveHeight: true
+});
+
+
+
+
+$('.dropdown-btn').on('click', function (e) {
+    if ($(this).hasClass('active')) { $('.dropdown-btn').removeClass('active').next().slideUp(); }
+    else {
+        $('.dropdown-btn').removeClass('active').next().slideUp();
+        $(this).toggleClass('active').next().slideToggle();
+    }
+});
