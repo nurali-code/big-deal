390c4a5cff821308cdda408593f22a7147a63756
nurali-code <nur3.dav.97@gmail.com>
2023-10-20 16:55:08 +0500

    request

--- js/script.js
+++ js/script.js
@@ -47,10 +47,46 @@ $('.reviews-slider').slick({
 $('.clients-slider').slick({
     dots: false,
     infinite: true,
+    swipeToSlide: true,
+    centerPadding: '',
     speed: 300,
     slidesToShow: 4,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                centerMode: true,
+            }
+        },
+        {
+            breakpoint: 920,
+            settings: {
+                centerPadding: '20px',
+                slidesToShow: 2,
+                variableWidth: true,
+                arrows: false,
+                centerMode: false,
+            }
+        },
+    ]
 });
+
+var scene2 = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2, {
+    relativeInput: true
+});
+
+
+$('[data-tab-btn]').click(function () {
+    var tabBtnValue = $(this).attr('data-tab-btn');
+    $('[data-tab-btn]').removeClass('active');
+    $(this).addClass('active');
+    $('[data-tab-content="' + tabBtnValue + '"]').addClass('active');
+    $('[data-tab-content]').not('[data-tab-content="' + tabBtnValue + '"]').removeClass('active');
+})
+
 
 $('.dropdown-btn').on('click', function (e) {
     if ($(this).hasClass('active')) { $('.dropdown-btn').removeClass('active').next().slideUp(); }
